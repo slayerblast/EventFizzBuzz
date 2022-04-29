2cea67e2a22b02542fd8e0e9177516e1ffd34a03
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-04-29 13:41:38 +0200

    test des fonctions dans le fichier test.js

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+let tab = ["fizz","fizzbuzz"]
+emitter.on("nombreReçu", function(eventArguments) {
+    eventArguments = 19
 
-emitter.on("messageReçu", function(eventArguments) {
-    console.log(eventArguments);
+    if((eventArguments % 3 == 0) && (eventArguments % 5 == 0))
+    console.log("fizzbuzz");
+
+    else if(eventArguments % 3 == 0)
+    console.log("fizz");
+    if(eventArguments % 5 == 0)
+    console.log("buzz");
+    else console.log(eventArguments);
 });
 
-emitter.emit("messageReçu","ceci est une string");
+emitter.emit("nombreReçu");
