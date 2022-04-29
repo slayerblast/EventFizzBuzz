8ae2f5f7a5381a1b7f0d0d59b7c5fe4a9a41763d
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-04-29 13:49:43 +0200

    ok : version final du la fonction fizzbuzz pret à etre copier coller dans le bon fichier

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
-let tab = ["fizz","fizzbuzz"]
-emitter.on("nombreReçu", function(eventArguments) {
-    eventArguments = 19
 
+emitter.on("nombreReçu", function(eventArguments) {
+    eventArguments = 30
     if((eventArguments % 3 == 0) && (eventArguments % 5 == 0))
-    console.log("fizzbuzz");
+   return console.log("fizzbuzz");
 
     else if(eventArguments % 3 == 0)
-    console.log("fizz");
+    return console.log("fizz");
     if(eventArguments % 5 == 0)
-    console.log("buzz");
-    else console.log(eventArguments);
+    return console.log("buzz");
+    else return console.log(eventArguments);
 });
 
 emitter.emit("nombreReçu");
